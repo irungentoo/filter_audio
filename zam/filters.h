fc7ba7734af5e2bf386c1af93ba966d1b9cae608
Damien Zammit <damien@zamaudio.com>
2015-04-11 02:15:08 +1000

    Add gate to duck mic when playback is loud

--- zam/filters.h
+++ zam/filters.h
@@ -1,5 +1,6 @@
 #include <inttypes.h>
 #include <math.h>
+#define MAX_GATE (8*480)
 
 typedef struct {
     double x[3];
@@ -8,8 +9,16 @@ typedef struct {
     double b[3];
 } FilterStateZam;
 
+typedef struct {
+    float samples[MAX_GATE];
+    float gain;
+    int pos;
+    float *playbuf;
+} Gate;
+
 void init_highpass_filter_zam(FilterStateZam *hpf, float fc, float fs);
 void init_lowpass_filter_zam(FilterStateZam *lpf, float fc, float fs);
 int run_filter_zam(FilterStateZam* fil, float* data, int length);
 int run_saturator_zam(float *data, int length);
+void run_gate(Gate *gate, float *playsignal, float *micsignal, float *outsignal, int frames, float fs);
 double sanitize_denormal(double v);
